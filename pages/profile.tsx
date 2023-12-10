1b904ecf2c7b987acd1b3aff7e7686b8d762aa73
Jessica Chen <jc939@duke.edu>
2023-12-09 19:58:56 -0500

    put limit on character

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -209,6 +209,15 @@ const ProfilePage: React.FC = () => {
     fetchData();
   }, []);
 
+  const handleNameChange = (e:any) => {
+    const inputValue = e.target.value;
+
+    const nameLimit = 70;
+    if (inputValue.length <= nameLimit) {
+      setName(inputValue);
+    }
+  };
+
   // useEffect(() => {
   //   const fetchData = async () => {
   //     try {
@@ -312,7 +321,7 @@ const ProfilePage: React.FC = () => {
                     type="text"
                     className="w-[20vw] text-5xl font-bold displayheader opacity-75"
                     value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    onChange={(e) => handleNameChange(e)}
                   />
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
